cf4d6564d4c0d84a92d2c678e7d18b7dba398df2
Asitha Navaratne <asithan476@gmail.com>
2024-11-18 22:03:37 +0530

    Minor fix to Stores loader

--- frontend/src/Views/StoresPage/StoresLoader.ts
+++ frontend/src/Views/StoresPage/StoresLoader.ts
@@ -1,27 +1,12 @@
-import ColumnsType from "./types/ApiColumnsType";
-
 import Service from "../../Services/StoreService";
 
 const { GetStores } = Service();
 
 const loader = async function () {
-  return GetStores()
-    .then((res) => {
-      return res.data.map((row: ColumnsType) => ({
-        id: row.id,
-        storeName: row.store_name,
-        storeAddress: row.store_address,
-        active: row.active,
-        createdBy: row.created_by,
-        createdOn: row.created_on,
-        updatedBy: row.updated_by,
-        updatedOn: row.updated_on,
-      }));
-    })
-    .catch((err) => {
-      // TODO: Handle errors properly
-      console.error(err);
-    });
+  return GetStores().catch((err) => {
+    // TODO: Handle errors properly
+    console.error(err);
+  });
 };
 
 export default loader;
