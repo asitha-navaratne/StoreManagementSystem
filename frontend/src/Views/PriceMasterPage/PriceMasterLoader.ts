14299cc2c31308d25305b157287a6ecc5b4c100e
Asitha Navaratne <asithan476@gmail.com>
2024-12-12 10:09:03 +0530

    Minor code cleanup

--- frontend/src/Views/PriceMasterPage/PriceMasterLoader.ts
+++ frontend/src/Views/PriceMasterPage/PriceMasterLoader.ts
@@ -2,13 +2,11 @@ import { QueryClient, queryOptions } from "@tanstack/react-query";
 
 import LoaderDataType from "./types/LoaderType";
 
-import PriceMasterService from "../../Services/PriceMasterService";
-import StoreService from "../../Services/StoreService";
-import SupplierService from "../../Services/SupplierService";
+import { getSuppliersQuery } from "../SuppliersPage/SuppliersLoader";
+import { getStoresQuery } from "../StoresPage/StoresLoader";
+import Service from "../../Services/PriceMasterService";
 
-const { GetPriceItems } = PriceMasterService();
-const { GetStores } = StoreService();
-const { GetSuppliers } = SupplierService();
+const { GetPriceItems } = Service();
 
 export const getPriceItemsQuery = queryOptions({
   queryKey: ["price items", "all"],
@@ -20,16 +18,8 @@ const loader = (queryClient: QueryClient) =>
     const payload: LoaderDataType = { products: [], stores: [], suppliers: [] };
 
     const priceItemsData = queryClient.ensureQueryData(getPriceItemsQuery);
-
-    const suppliersData = queryClient.ensureQueryData({
-      queryKey: ["suppliers", "all"],
-      queryFn: GetSuppliers,
-    });
-
-    const storesData = queryClient.ensureQueryData({
-      queryKey: ["stores", "all"],
-      queryFn: GetStores,
-    });
+    const suppliersData = queryClient.ensureQueryData(getSuppliersQuery);
+    const storesData = queryClient.ensureQueryData(getStoresQuery);
 
     payload["products"] = await priceItemsData;
     payload["suppliers"] = await suppliersData;
